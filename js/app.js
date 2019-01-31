ed638274af5cce4629250b96834a778ff1aefa28
000hashbrown000 <hashbrown@arrrrgggg>
2019-01-31 11:50:42 -0800

    Removed all old calls
    Added Vue and required calls to READONLY api key

--- js/app.js
+++ js/app.js
@@ -1,46 +1,31 @@
 $(document).foundation();
-// Render the PayPal button
-
-paypal.Button.render({
-
-    // Set your environment
-
-    env: 'sandbox', // sandbox | production
-
-    // Specify the style of the button
-
-    style: {
-        label: 'pay',
-        size:  'medium',    // small | medium | large | responsive
-        shape: 'pill',     // pill | rect
-        color: 'white',      // gold | blue | silver | black
-        tagline: 'false'
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: []
     },
-
-    // PayPal Client IDs - replace with your own
-    // Create a PayPal app: https://developer.paypal.com/developer/applications/create
-
-    client: {
-        sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
-        production: '<insert production client id>'
+    mounted: function(){
+       this.loadItems();
     },
+    methods: {
+        loadItems: function(){
 
-    payment: function(data, actions) {
-        return actions.payment.create({
-            payment: {
-                transactions: [
-                    {
-                        amount: { total: '0.01', currency: 'USD' }
-                    }
-                ]
-            }
-        });
-    },
-
-    onAuthorize: function(data, actions) {
-        return actions.payment.execute().then(function() {
-            window.alert('Payment Complete!');
-        });
+            // Init variables
+            var self = this
+            var app_id = "appCsH7ytW2fUJCOU";
+            var app_key = "keyLBt6WtuDTOaZ72";
+            var table_name = "Inventory"
+            this.items = []
+            axios.get(
+                "https://api.airtable.com/v0/"+app_id+"/"+table_name+"?view=Grid%20view",
+                {
+                    headers: { Authorization: "Bearer "+app_key }
+                }
+            ).then(function(response){
+                self.items = response.data.records
+            }).catch(function(error){
+                console.log(error)
+            })
+        }
     }
-
-}, '#paypal-button-container');
+})
